290202be9407d69c006d4faf247ef4bd424d5356
davidefois96 <davidefois360@gmail.com>
2024-03-11 15:41:34 +0100

    [add] new code

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+
+const {createApp}= Vue;
+
+
+createApp({
+
+  
+  data(){
+    
+    return{
+
+      message:'hello world',
+      myUrl: 'https://www.sardegna.info/wp-content/uploads/sites/5/portoscuso.jpg',
+
+    }
+
+  }
+
+
+}).mount('#app');
---
+++ README.md
@@ -0,0 +1,9 @@
+VUE-HELLO
+===
+
+## Elementi della consegna
+
+1. Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+2. Bonus:Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
